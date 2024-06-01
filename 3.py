bca61a8d98ee394f2b2394364a010607def6449d
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2024-06-01 15:09:59 +0200

    solved 3

---
+++ 3.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim: ft=python ts=4 sw=4 sts=4 et fenc=utf-8
+# Original author: "Eivind Magnus Hvidevold" <hvidevold@gmail.com>
+# License: GNU GPLv3 at http://www.gnu.org/licenses/gpl.html
+
+'''
+'''
+
+import os
+import sys
+import re
+from sympy import primefactors
+
+def main():
+    'entry point'
+    r = 600851475143
+    prod = primefactors(r)
+    print(max(prod))
+
+if __name__ == '__main__':
+    main()
+
